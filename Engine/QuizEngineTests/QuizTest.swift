d64a3a5fd1bc77daa59ae9ac8ce8ae849a373412
frankm <117271+frankm@users.noreply.github.com>
2023-05-01 19:50:51 -0700

    introduce new Quiz.start APIs

--- Engine/QuizEngineTests/QuizTest.swift
+++ Engine/QuizEngineTests/QuizTest.swift
@@ -4,52 +4,73 @@
 
 import Foundation
 import XCTest
-import QuizEngine
+@testable import QuizEngine
+
+final class Quiz {
+    private let flow: Any
+
+    private init(flow: Any) {
+        self.flow = flow
+    }
+    
+    static func start<Question, Answer: Equatable, Delegate: QuizDelegate>(questions: [Question], delegate: Delegate, correctAnswers: [Question: Answer]) -> Quiz where Delegate.Question == Question, Delegate.Answer == Answer {
+        let flow = Flow(questions: questions, delegate: delegate, scoring: { scoring($0, correctAnswers: correctAnswers) })
+        flow.start()
+        return Quiz(flow: flow)
+    }
+}
 
 class QuizTest: XCTestCase {
 
-    private let router = RouterSpy()
-    private var game: Game<String, String, RouterSpy>!
+    private let delegate = DelegateSpy()
+    private var quiz: Quiz!
 
     override func setUp() {
         super.setUp()
 
-        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+        quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate, correctAnswers: ["Q1": "A1", "Q2": "A2"])
     }
 
-    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
-        router.answerCallback("wrong")
-        router.answerCallback("wrong")
+    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+        delegate.answerCallback("wrong")
+        delegate.answerCallback("wrong")
 
-        XCTAssertEqual(router.routedResult!.score, 0)
+        XCTAssertEqual(delegate.handledResult!.score, 0)
     }
 
-    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
-        router.answerCallback("A1")
-        router.answerCallback("wrong")
+    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
+        delegate.answerCallback("A1")
+        delegate.answerCallback("wrong")
 
-        XCTAssertEqual(router.routedResult!.score, 1)
+        XCTAssertEqual(delegate.handledResult!.score, 1)
     }
 
-    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
-        router.answerCallback("A1")
-        router.answerCallback("A2")
+    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        delegate.answerCallback("A1")
+        delegate.answerCallback("A2")
 
-        XCTAssertEqual(router.routedResult!.score, 2)
+        XCTAssertEqual(delegate.handledResult!.score, 2)
     }
 
-    private class RouterSpy: Router {
-        var routedQuestions: [String] = []
-        var routedResult: Result<String, String>? = nil
-
+    private class DelegateSpy: Router, QuizDelegate {
+        var handledResult: Result<String, String>? = nil
         var answerCallback: (String) -> Void = { _ in }
+        //        var routedQuestions: [String] = []
 
-        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        func handle(question: String, answerCallback: @escaping (String) -> Void) {
             self.answerCallback = answerCallback
         }
 
+        func handle(result: Result<String, String>) {
+            handledResult = result
+        }
+
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            handle(question: question, answerCallback: answerCallback)
+        }
+
         func routeTo(result: Result<String, String>) {
-            routedResult = result
+            handle(result: result)
         }
     }
 

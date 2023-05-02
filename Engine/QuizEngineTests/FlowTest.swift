6e97f1a039c212101d7462f4ac8326f525f5643b
frankm <117271+frankm@users.noreply.github.com>
2023-05-02 13:31:15 -0700

    S2E8 start Flow w/0 questions completes w/empty quiz for new API

--- Engine/QuizEngineTests/FlowTest.swift
+++ Engine/QuizEngineTests/FlowTest.swift
@@ -66,10 +66,12 @@ class FlowTest: XCTestCase {
         XCTAssertTrue(delegate.completedQuizzes.isEmpty)
     }
 
-    func test_start_withNoQuestions_delegatesToResultHandling() {
+    func test_start_withNoQuestions_completeWithEmptyQuiz() {
         makeSUT(questions: []).start()
         
-        XCTAssertEqual(delegate.handledResult!.answers, [:])
+        XCTAssertEqual(delegate.completedQuizzes.count, 1)
+        XCTAssertTrue(delegate.completedQuizzes[0].isEmpty)
+
     }
     
     func test_startAndAnswerFirstQuestion_withTwoQuestions_doesNotDelegateToResultHandling() {
@@ -138,13 +140,17 @@ class FlowTest: XCTestCase {
     private class DelegateSpy: QuizDelegate {
         var handledQuestions: [String] = []
         var handledResult: Result<String, String>? = nil
-        var completedQuizzes = [(String, String)]()
+        var completedQuizzes = [[(String, String)]]()
 
         var answerCompletion: (String) -> Void = { _ in }
 
         func answer(for question: String, completion: @escaping (String) -> Void) {
             handledQuestions.append(question)
             self.answerCompletion = completion
+        }
+
+        func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
+            completedQuizzes.append(answers)
         }
 
         func handle(result: Result<String, String>) {

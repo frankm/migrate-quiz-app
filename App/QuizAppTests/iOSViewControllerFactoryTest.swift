f0106b7d762c90f87cab542552adf312d95c1d9a
frankm <117271+frankm@users.noreply.github.com>
2023-05-04 18:24:40 -0700

    introduce new factory API to create result view controller

--- App/QuizAppTests/iOSViewControllerFactoryTest.swift
+++ App/QuizAppTests/iOSViewControllerFactoryTest.swift
@@ -82,19 +82,16 @@ class iOSViewControllerFactoryTest: XCTestCase {
     func makeResults() -> (controller: ResultsViewController, presenter: ResultsPresenter) {
         let userAnswers = [(singleAnswerQuestion, ["A1"]), (multipleAnswerQuestion, ["A1", "A2"])]
         let correctAnswers = [(singleAnswerQuestion, ["A1"]), (multipleAnswerQuestion, ["A1", "A2"])]
-        let result = Result.make(
-            answers: [singleAnswerQuestion: ["A1"], multipleAnswerQuestion: ["A1", "A2"]],
-            score: 2)
 
         let presenter = ResultsPresenter(
             userAnswers: userAnswers,
             correctAnswers: correctAnswers,
-            scorer: { _, _ in result.score}
+            scorer: BasicScore.score
         )
 
         let sut = makeSUT(correctAnswers: correctAnswers)
         
-        let controller = sut.resultsViewController(for: result) as! ResultsViewController
+        let controller = sut.resultsViewController(for: userAnswers) as! ResultsViewController
         
         return (controller, presenter)
     }
